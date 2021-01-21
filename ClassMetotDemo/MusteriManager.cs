04f7c3323193841a4dc7e42abf0c13f30da235b7
sümeyra sağır <sumeyrasagir2017@outlook.com>
2021-01-21 19:30:06 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(string musteriAd, string musteriSoyad, int Yas, string TelNo, int Id, int Bakiye)
+        {
+            Console.WriteLine("Yeni müşteri veritabanına eklendi  !");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Seçilen müşteri silindi: " + musteri.Id);
+        }
+
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Müşterinin adı: " + musteri.Ad);
+                Console.WriteLine("Müşterinin soyadı: " + musteri.Soyad);
+                Console.WriteLine("Müşterinin yaşı: " + musteri.Yas);
+                Console.WriteLine("Müşterinin telefon numarası: " + musteri.TelNo);
+                Console.WriteLine("Müşteri Id: " + musteri.Id);
+                Console.WriteLine("Müşteri bakiyesi: " + musteri.Bakiye);
+                Console.WriteLine("-----------------------------------------");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri ();
+            musteri1.Ad = "Sümeyra";
+            musteri1.Soyad = "Sağır";
+            musteri1.Yas = 22;
+            musteri1.TelNo = "05001231400";
+            musteri1.Id = 123456789;
+            musteri1.Bakiye = 27000;
+
+            Musteri musteri2 = new Musteri ();
+            musteri2.Ad = "Kübra";
+            musteri2.Soyad = "Eser";
+            musteri2.Yas = 26;
+            musteri2.TelNo = "05004568978";
+            musteri2.Id = 852147963;
+            musteri2.Bakiye = 65000;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2  };
+
+            MusteriManager musteriManagers = new MusteriManager();
+
+            musteriManagers.Listele(musteriler);
+
+            musteriManagers.Ekle("Rüveyda", "Kayabaşı", 23, "512347896", 546123897, 43000);
+
+            musteriManagers.Sil(musteri2);
+
+            
+
+
+        }
+    }
+}
